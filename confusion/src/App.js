0e8cd6903ac2dda55c78c7377e9276ea77346f1b
Lagan Rai <laganrai500@gmail.com>
2022-01-22 21:40:54 +0530

    forms

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,20 +1,21 @@
-import React , { Component } from 'react';
-import logo from './logo.svg';
-import { Navbar, NavbarBrand } from 'reactstrap';
+import React, { Component } from 'react';
+import Main from './components/MainComponent';
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
 
-function App() {
-  return (
-        <div className="App">
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
+
+
+class App extends Component {
+
+  render() {
+    return (
+     <BrowserRouter>
+      <div>
+        <Main />
       </div>
+      </BrowserRouter>
     );
+  }
 }
-
-
 
 export default App;
